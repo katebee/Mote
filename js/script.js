5a717b1ef19ae77bfc0478cf1910292fd796847f
Kate Whalen <whalenka88@gmail.com>
2015-07-16 10:03:57 +0100

    new canvas object added

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ function draw() {
     var mote = new Path2D();
     ctx.beginPath();
     mote.arc(400, 200, 50, 0, Math.PI * 2, true); // Outer circle
-
+//create function draw circle
     ctx.closePath();
     //mote.arc(75, 75, 60, 0, Math.PI * 2, true);
     ctx.fillStyle = "rgba(166,222,222,1)";
@@ -17,10 +17,19 @@ function draw() {
     ctx.fill(mote);
 
     var pulse = new Path2D();
+    ctx.beginPath();
+    pulse.arc(400, 200, 70, 0, Math.PI * 2, true); // Outer circle
 
+    ctx.closePath();
+    //pulse.arc(75, 75, 60, 0, Math.PI * 2, true);
+    //ctx.fillStyle = "rgba(166,222,222,1)";
+    ctx.strokeStyle = "rgba(220,111,201,1)";
+    ctx.lineWidth = 15;
+    ctx.stroke(pulse);
+    //ctx.fill(pulse);
   }
 }
-
+//use timeOut for 20ms to refresh
 $(document).ready(function() {
     console.log("document is ready");
   $("#start").on("click", draw());
