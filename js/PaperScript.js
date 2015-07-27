6563b1cef3f4413b82db4cd144a162cb99987c7e
katebee <whalenka88@gmail.com>
2015-07-27 17:04:20 +0100

    Small edits, also added clock module

---
+++ js/PaperScript.js
@@ -0,0 +1,21 @@
+
+
+var canvas;
+var stage;
+var mote;
+var halo;
+
+function createEasel() {
+  console.log("making an easel in the canvas");
+  // create a new stage and point it at our canvas:
+  canvas = document.getElementById('mote-canvas');
+  stage = new createjs.Stage(canvas);
+
+  var mote = new createjs.Shape();
+  mote.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
+  mote.x = 100;
+  mote.y = 100;
+  stage.addChild(circle);
+
+  stage.update();
+}
--- js/mote.js
+++ js/mote.js
@@ -1,19 +1,57 @@
+//MODULE FOR CANVAS
+
+function responsiveCanvas() {
+  var canvasWidth = $(window).width() - 50;
+  console.log("window width is: " + canvasWidth);
+  var canvasArea = $("#mote-canvas").height() * canvasWidth;
+  //if (canvasWidth < 800) {
+
+  //}
+
+}
+
+//MODULE FOR CLOCK - keeping clock components confined to local scope
+
+var Clock = (function () {
+  var displayTime = function() {
+      var currentTime = new Date();
+      var hours = currentTime.getHours();
+      var minutes = currentTime.getMinutes();
+      var seconds = currentTime.getSeconds();
+
+      var clockDiv = document.getElementById('clock');
+      clockDiv.innerText = hours + ":" + minutes + ":" + seconds;
+    }
+
+    return {
+    displayTime: displayTime //public method
+  };
+}()); // Immediately-Invoked-Function-Expression
 
 
-//SCRIPT FOR CANVAS DISPLAY
+
+//SCRIPT FOR CANVAS DISPLAY - PHENOTYPE / GENE EXPRESSION
 
 function draw() {
   console.log("starts at draw");
-  var canvas = document.getElementById('canvas');
+
+  var canvas = document.getElementById('mote-canvas');
   if (canvas.getContext) {
     var ctx = canvas.getContext('2d');
-    //var canvasHeight = ;
-    //var canvasWidth = ;
+  /*  var canvasHeight = canvas.height();
+    var canvasWidth = canvas.width(); */
+
+    var pulse = new Path2D();
+    ctx.beginPath();
+    pulse.arc(400, 300, 160, 0, Math.PI * 2, true); // Outer circle
+    ctx.closePath();
+    ctx.fillStyle = "rgba(220,111,201,0.5)"; //get chromophore func return value
+    ctx.lineWidth = 15;
+    ctx.fill(pulse);
 
     var mote = new Path2D();
     ctx.beginPath();
-    mote.arc(400, 200, 50, 0, Math.PI * 2, true); // Outer circle
-//create function draw circle
+    mote.arc(400, 300, 120, 0, Math.PI * 2, true); // Body of mote
     ctx.closePath();
     //mote.arc(75, 75, 60, 0, Math.PI * 2, true);
     ctx.fillStyle = "rgba(166,222,222,1)";
@@ -21,27 +59,36 @@ function draw() {
     ctx.lineWidth = 15;
     ctx.stroke(mote);
     ctx.fill(mote);
-
-    var pulse = new Path2D();
-    ctx.beginPath();
-    pulse.arc(400, 200, 70, 0, Math.PI * 2, true); // Outer circle
-
-    ctx.closePath();
-    //pulse.arc(75, 75, 60, 0, Math.PI * 2, true);
-    //ctx.fillStyle = "rgba(166,222,222,1)";
-    ctx.strokeStyle = "rgba(220,111,201,1)";
-    ctx.lineWidth = 15;
-    ctx.stroke(pulse);
-    //ctx.fill(pulse);
-  }
+  };
 }
+
 //use timeOut for 20ms to refresh
+
+
+
+//--------------------- main ---------------------
 
 $(document).ready(function() {
 
   console.log("document is ready");
-  $("#start").on("click", draw());
-  $('#title-link').smoothScroll({offset: -40});
+
+  //Add the clock and update each second
+  Clock.displayTime();
+  setInterval(Clock.displayTime, 1000);
+
+
+  draw();
+
+  //$("#start").on("click", draw());
+  $('#title-link').smoothScroll({offset: -109});
   $('#about-link').smoothScroll({offset: -40});
   $('#contact-link').smoothScroll({offset: -40});
+
+  $(window).resize(function() {
+    var windowWidth = $(window).width() - 50;
+    console.log("window width is: " + windowWidth);
+    var canvasArea = $("#mote-canvas").height() * $("#mote-canvas").width();
+    console.log("canvas area is: " + canvasArea);
+
+  });
 });
